f6ea3e74eda198f3bafe850dc2d33ed285d8caf6
Sapavat-A <sapavatanjianji44@gmail.com>
2026-04-07 21:35:57 +0530

    update

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,4 +1,16 @@
-class Config:
-    SECRET_KEY = "secret"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# class Config:
+    # SECRET_KEY = "secret"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend is running!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
